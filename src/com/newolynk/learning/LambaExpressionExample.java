c0792fa90a142e09ff4854e6a7902b26628e8b9d
Achraf Ben Younes <achrafbenyounes2012@gmail.com>
2016-11-12 12:37:57 +0100

    Defining a generic interface for arithmetic operation

---
+++ src/com/newolynk/learning/LambaExpressionExample.java
@@ -0,0 +1,22 @@
+package com.newolynk.learning;
+
+public class LambaExpressionExample {
+
+	public static void main(String[] args) {
+    
+		MathOperation addition;
+		
+		MathOperation substruction;
+		
+		MathOperation Multiplication;
+		
+		MathOperation Division;
+
+
+	}
+	
+	interface MathOperation {
+		int operation(int a, int b);
+	}
+
+}
