abd303367c38a617a228303958e27c6f71fe92b0
Achraf Ben Younes <achrafbenyounes2012@gmail.com>
2016-11-14 22:43:18 +0100

    switch between time zone

--- src/com/newolynk/learning/TimeAPI.java
+++ src/com/newolynk/learning/TimeAPI.java
@@ -4,11 +4,14 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 public class TimeAPI {
    public static void main(String args[]){
 	   TimeAPI timeApiTester = new TimeAPI();
 	   timeApiTester.testLocalDateTime();
+	   timeApiTester.testZonedDateTime();
    }
 	
    public void testLocalDateTime(){
@@ -40,5 +43,18 @@ public class TimeAPI {
       //parse a string
       LocalTime date5 = LocalTime.parse("20:15:30");
       System.out.println("date5: " + date5);
+   }
+   
+   public void testZonedDateTime(){
+		
+	   // Get the current date and time
+	   ZonedDateTime date1 = ZonedDateTime.parse("2007-12-03T10:15:30+05:30[Asia/Karachi]");
+	   System.out.println("date1: " + date1);
+			
+	   ZoneId id = ZoneId.of("Europe/Paris");
+	   System.out.println("ZoneId: " + id);
+			
+	   ZoneId currentZone = ZoneId.systemDefault();
+	   System.out.println("CurrentZone: " + currentZone);
    }
 }
